3da9187ff664d9c09ab934f33138177b7c9a5a91
FRANGONICOLAS <nicolas.gonzalez_fra@uao.edu.co>
2025-09-16 02:48:32 -0500

    Creación de la gestion de acudientes v1.0. Se espera conexion con frontend. Se realizaron cambios en el POM por problemas en la ejecucion y desactivacion de liquidbase ya que generaba muchos conflictos para el debug de la app.

---
+++ backend/src/main/java/uao/edu/co/scouts_project/acudientes/service/AcudienteService.java
@@ -0,0 +1,49 @@
+package uao.edu.co.scouts_project.acudientes.service;
+
+import uao.edu.co.scouts_project.acudientes.entity.Acudiente;
+import uao.edu.co.scouts_project.acudientes.repository.AcudienteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class AcudienteService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AcudienteService.class);
+
+    @Autowired
+    private AcudienteRepository acudienteRepository;
+
+    public List<Acudiente> findAll() {
+        logger.info("Acudientes listados");
+        return acudienteRepository.findAll();
+    }
+
+    public Acudiente saveAcudiente (Acudiente acudiente) {
+        logger.info("Acudiente creado");
+        return acudienteRepository.save(acudiente);
+    }
+
+    public void deleteAcudiente(Long id) {
+        logger.info("Acudiente eliminado");
+        acudienteRepository.deleteById(id);
+    }
+
+    public Acudiente updateAcudiente(Long id, Acudiente acudiente) {
+        Acudiente existingAcudiente = acudienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Acudiente not found"));
+
+        existingAcudiente.setAcudienteName(acudiente.getAcudienteName());
+        existingAcudiente.setAcudienteLastname(acudiente.getAcudienteLastname());
+        existingAcudiente.setAcudientePhone(acudiente.getAcudientePhone());
+        existingAcudiente.setAcudienteParent(acudiente.getAcudienteParent());
+        existingAcudiente.setAcudienteEmail(acudiente.getAcudienteEmail());
+
+        logger.info("Acudiente actualizado");
+
+        return acudienteRepository.save(existingAcudiente);
+    }
+}
