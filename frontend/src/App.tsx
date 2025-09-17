ba766d0d1de19f08ffb02481af09f52abd0efb2e
Monica <monicachicangana0317@gmail.com>
2025-09-17 00:05:54 -0500

    pagina de inicio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./app/routes/Login";
 import Register from "./app/routes/Register";
 import ScoutEnrollment from "./app/routes/ScoutEnrollment";
+import Home from "./app/routes/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="h-screen w-screen">
         <Routes>
+          <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/inscription" element={<ScoutEnrollment />} />
-          {/* Agrega más rutas aquí */}
         </Routes>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
