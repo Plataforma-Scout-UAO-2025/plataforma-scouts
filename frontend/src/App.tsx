628ee3aa4624c93bbde9900d49b849ffe188c27b
sergio <sergioandresherreravallejo@gmail.com>
2025-09-20 00:45:18 -0500

    Restauro frontend desde develop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Routes imports
 import Login from "./app/routes/Login";
 import Register from "./app/routes/Register";
+import AppLayout from "./components/layout/AppLayout";
+import Cuotas from "./app/routes/financiero/Cuotas/Cuotas";
+import Dashboard from "./app/routes/Dashboard";
+import Gestion from "./app/routes/financiero/Gestion/Gestion";
+import { Toaster } from "sonner";
+import MedicalInfo from "./app/routes/grupos/medical-info/MedicalInfo";
+import Grupos from "./app/routes/grupos/Grupos";
 
 function App() {
   return (
@@ -11,9 +18,19 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/app" element={<AppLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="financiero/cuotas" element={<Cuotas />} />
+
+            <Route path="financiero/cuotas/gestion" element={<Gestion />} />
+            <Route path="grupos" element={<Grupos />} />
+            <Route path="grupos/medical-info" element={<MedicalInfo />} />
+
+          </Route>
           {/* Agrega más rutas aquí */}
         </Routes>
       </div>
+      <Toaster />
     </BrowserRouter>
   );
 }
