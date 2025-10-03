cfc06e275f5178ed14e22690ed1ccfde2a8632b7
Etienne Samboni <etiennesamboni@gmail.com>
2025-10-03 13:48:42 -0500

    refactor: remove unused useApi hook and simplify axios instance creation

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,37 +1,10 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
-import { useMemo } from "react";
 
-export const useApi = () => {
-  const { getAccessTokenSilently } = useAuth0();
+const api = axios.create({
+  baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL || "http://localhost:8080/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-  const api = useMemo(() => {
-    const instance = axios.create({
-      baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL || "http://localhost:8080/api/v1",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    instance.interceptors.request.use(async (config) => {
-      const token = await getAccessTokenSilently();
-      if (token) config.headers.Authorization = `Bearer ${token}`;
-      return config;
-    });
-
-    instance.interceptors.response.use(
-      (response) => response,
-      (error) => {
-        if (error.response?.status === 401) {
-          window.location.href = "/login";
-        }
-        return Promise.reject(error);
-      }
-    );
-
-    return instance;
-  }, [getAccessTokenSilently]);
-
-  return api;
-};
-export default useApi;
+export default api;
