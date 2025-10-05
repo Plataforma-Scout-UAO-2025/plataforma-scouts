f825870d588fc3375c28492e2467d3737ce9dd28
sergio <sergioandresherreravallejo@gmail.com>
2025-10-04 22:43:59 -0500

    Fix: Added to the main.tsx file the audience solitude from the frontend .env

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,8 @@ createRoot(document.getElementById('root')!).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin + '/app'
+        redirect_uri: window.location.origin + '/app',
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       }}
     >
       <App />
