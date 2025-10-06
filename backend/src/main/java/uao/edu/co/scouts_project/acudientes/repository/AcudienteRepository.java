b8e2b30f620eaef0b183764a9c13bc3a81675123
andres <afgomez0622@gmail.com>
2025-10-05 19:34:49 -0500

    🔥chore: remove deprecated code

--- backend/src/main/java/uao/edu/co/scouts_project/acudientes/repository/AcudienteRepository.java
+++
@@ -1,7 +0,0 @@
-package uao.edu.co.scouts_project.acudientes.repository;
-
-import uao.edu.co.scouts_project.acudientes.entity.Acudiente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcudienteRepository extends JpaRepository<Acudiente, Long> {
-}
