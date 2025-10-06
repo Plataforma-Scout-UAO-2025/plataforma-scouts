cca82818b232341a02221208fa3e40360805fa0f
andres <afgomez0622@gmail.com>
2025-10-06 18:50:33 -0500

    🔥refactor: remove deprecated code

--- backend/src/main/java/uao/edu/co/scouts_project/guardian/service/GuardianService.java
+++ backend/src/main/java/uao/edu/co/scouts_project/guardian/service/GuardianService.java
@@ -4,22 +4,10 @@ import java.util.List;
 
 import uao.edu.co.scouts_project.guardian.dto.GuardianCreateDTO;
 import uao.edu.co.scouts_project.guardian.dto.GuardianDTO;
-import uao.edu.co.scouts_project.guardian.dto.MemberCreateDTO;
-import uao.edu.co.scouts_project.guardian.dto.MemberResponseDTO;
 import uao.edu.co.scouts_project.guardian.dto.MemberSummaryDTO;
 
 public interface GuardianService {
-    // Métodos generales para Member
-    List<MemberResponseDTO> findAll();
-    MemberResponseDTO findMemberById(String id);
-    List<MemberResponseDTO> findMemberByStatus(boolean active);
-    MemberResponseDTO saveMember(MemberCreateDTO memberCreateDTO);
-    MemberResponseDTO updateMemberById(String memberId, MemberCreateDTO memberCreateDTO);
-    MemberResponseDTO updateMemberRole(String memberId, String newRole);
-    MemberResponseDTO updateMemberStatus(String memberId, boolean isActive);
-    boolean deleteMemberById(String memberId);
-
-    // Métodos específicos para Guardian (mismo servicio)
+    
     List<GuardianDTO> findAllGuardians();
     GuardianDTO findGuardianById(String id);
     List<GuardianDTO> findGuardiansByStatus(boolean active);
