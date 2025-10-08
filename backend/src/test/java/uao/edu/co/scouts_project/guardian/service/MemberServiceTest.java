a11f9de5a79cb7e1af215ef5f0a48ac222ab811d
andres <afgomez0622@gmail.com>
2025-10-07 21:18:09 -0500

    ⚰️ chore: remove unused file

--- backend/src/test/java/uao/edu/co/scouts_project/guardian/service/MemberServiceTest.java
+++
@@ -1,9 +0,0 @@
-package uao.edu.co.scouts_project.guardian.service;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-class MemberServiceTest {
-
-}
