97ee85aef6bc53beddc9bab4ea30d0cb2b202d44
ioaNN2394 <johansa2394@gmail.com>
2025-10-10 14:48:56 -0500

    DELETE unused import and change tenat_id type from LONG to STRING

--- backend/src/main/java/uao/edu/co/scouts_project/organigrama/service/SectionService.java
+++ backend/src/main/java/uao/edu/co/scouts_project/organigrama/service/SectionService.java
@@ -22,7 +22,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.UUID;
-import java.time.Instant;
 
 @Service
 public class SectionService {
