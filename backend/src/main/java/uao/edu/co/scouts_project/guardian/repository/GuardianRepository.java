b20f635d130a2b507f2b6bc2336fc7b2af05058f
andres <afgomez0622@gmail.com>
2025-10-12 17:58:21 -0500

    ♻️ refactor: update queries to drop logic in service layer

--- backend/src/main/java/uao/edu/co/scouts_project/guardian/repository/GuardianRepository.java
+++ backend/src/main/java/uao/edu/co/scouts_project/guardian/repository/GuardianRepository.java
@@ -1,23 +1,39 @@
 package uao.edu.co.scouts_project.guardian.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import uao.edu.co.scouts_project.guardian.model.Member;
 import uao.edu.co.scouts_project.guardian.model.MemberCustom;
 
 @Repository
-public interface GuardianRepository extends JpaRepository<Member, String> {
+public interface GuardianRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByIsActive(boolean active);
-    //TODO: create a method that follows this structure
-    @Query("SELECT new uao.edu.co.scouts_project.guardian.model.MemberCustom(m.memberId, m.firstName, m.lastName, m.gender, m.phone, m.birthDate) " +
+
+    @Query("SELECT new uao.edu.co.scouts_project.guardian.model.MemberCustom(" +
+            "m.firstName, m.lastName, m.identification, m.documentType, m.emergencyContacts, " +
+            "m.age, m.gender, m.phone, m.birthDate) " +
             "FROM Member m WHERE m.guardianId = :guardianId")
-    List<MemberCustom> findMembersImInChargeOf(@Param("guardianId") Integer guardianId); //in the service we apply the mapping
-    //logic create in the model package a new model that captures this custom query value
+    List<MemberCustom> findMembersInChargeOf(Long guardianId);
+
+    @Query("SELECT m FROM Member m WHERE m.memberId = :id AND m.role = 'ACUDIENTE' AND m.status = 'APPROVED' AND m.isActive = true")
+    Optional<Member> findValidGuardianById(@Param("id") Long id);
+
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM Member m WHERE m.memberId = :id AND m.role = 'ACUDIENTE'")
+    boolean existsByIdAndHasRoleAcudiente(@Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE Member m SET m.guardianId = NULL WHERE m.guardianId = :guardianId")
+    void deleteGuardianIdFromMember(Long guardianId);
+
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM Member m WHERE m.identification = :identification AND m.role = 'ACUDIENTE' AND m.status = 'APPROVED' AND m.isActive = true")
+    boolean existsByValidGuardianIdentification(@Param("identification") String identification);
+
 }
