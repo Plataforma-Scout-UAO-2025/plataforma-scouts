495522d69b6e75538a97635938334590123acbb2
sergio <sergioandresherreravallejo@gmail.com>
2025-10-13 09:58:33 -0500

    fix: fixed duplicate security config endpoints

--- backend/src/main/java/uao/edu/co/scouts_project/infrastructure/security/SecurityConfig.java
+++ backend/src/main/java/uao/edu/co/scouts_project/infrastructure/security/SecurityConfig.java
@@ -50,14 +50,6 @@ public class SecurityConfig {
                                                 .hasAnyRole(ADMIN_GLOBAL.name())
                                                 .requestMatchers(HttpMethod.DELETE, "/api/v1/tenants/*")
                                                 .hasAnyRole(ADMIN_GLOBAL.name())
-
-                                                .requestMatchers(HttpMethod.POST, "/api/v1/tenants")
-                                                .hasAnyRole(ADMIN_GLOBAL.name(), DEV_SUPPORT.name())
-                                                .requestMatchers(HttpMethod.PUT, "/api/v1/tenants/*")
-                                                .hasAnyRole(ADMIN_GLOBAL.name(), DEV_SUPPORT.name())
-                                                .requestMatchers(HttpMethod.DELETE, "/api/v1/tenants/*")
-                                                .hasAnyRole(ADMIN_GLOBAL.name(), DEV_SUPPORT.name())
-
                                                 // Operaciones CRUD en grupos
 
                                                 .requestMatchers(HttpMethod.POST, "/api/v1/tenants/*/groups/**")
@@ -76,11 +68,6 @@ public class SecurityConfig {
                                                 // Operaciones de consulta en grupos
 
                                                 .requestMatchers(HttpMethod.GET, "/api/v1/tenants/*/groups/**")
-                                                .hasAnyRole(ADMIN_GLOBAL.name(), ADMIN_GRUPO.name(),
-                                                                COMITE_ADMIN.name(), DEV_SUPPORT.name(),
-                                                                SCOUTER.name(), TESORERO.name(), ACUDIENTE.name(),
-                                                                SCOUT.name())
-                                                .requestMatchers(HttpMethod.GET, "/api/v1/tenants")
                                                 .hasAnyRole(ADMIN_GLOBAL.name(), ADMIN_GRUPO.name(),
                                                                 COMITE_ADMIN.name(), DEV_SUPPORT.name(),
                                                                 SCOUTER.name(), TESORERO.name(), ACUDIENTE.name(),
