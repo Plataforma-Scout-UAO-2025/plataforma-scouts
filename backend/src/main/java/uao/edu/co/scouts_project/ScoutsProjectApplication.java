3c29804a5aaa8825980a30fd141986a39bf7e726
Diego Armando Santacruz <diego.santacruz@siesa.com>
2025-10-13 21:35:24 -0500

    Cambios temporales por despliegue

--- backend/src/main/java/uao/edu/co/scouts_project/ScoutsProjectApplication.java
+++ backend/src/main/java/uao/edu/co/scouts_project/ScoutsProjectApplication.java
@@ -2,8 +2,6 @@ package uao.edu.co.scouts_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
