3c60269536a905d975b21d88ebf86d163a10e9a1
Camilo Franco Moya <69817111+cfmoya324@users.noreply.github.com>
2025-10-16 23:13:03 -0500

    hotfix / compatibilidad de la página de registros médicos con deploy (#97)
    * posible fix para deploy de registros médicos
    
    * Arreglo de bug en dosis de medicamentos y tipos de datos
    
    * Refactorización de código

--- backend/src/main/java/uao/edu/co/scouts_project/medical/record/dto/MedicationDTO.java
+++ backend/src/main/java/uao/edu/co/scouts_project/medical/record/dto/MedicationDTO.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 public record MedicationDTO(
         @NotBlank(message = "Medication name is required")
         String name,
+        @NotBlank(message = "Medication dose is required")
+        String dose,
         @NotBlank(message = "Medication frequency is required")
         String frequency
 ) {}
