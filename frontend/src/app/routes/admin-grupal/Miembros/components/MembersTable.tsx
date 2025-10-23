13e02fbc8c67b00bc0952a3022e2e2d849869cdf
JuanJaramillo12004 <juan_edu.jaramillo@uao.edu.co>
2025-10-23 17:05:53 -0500

    fix: error en MemberTable

--- frontend/src/app/routes/admin-grupal/Miembros/components/MembersTable.tsx
+++ frontend/src/app/routes/admin-grupal/Miembros/components/MembersTable.tsx
@@ -165,7 +165,7 @@ const MembersTable = ({ filteredMembers }: MembersTableProps) => {
               </strong>{" "}
               a{" "}
               <strong>
-                {selectedMember && selectedMember.isActive
+                {selectedMember && isActive(selectedMember)
                   ? "INACTIVO"
                   : "ACTIVO"}
               </strong>
@@ -175,7 +175,7 @@ const MembersTable = ({ filteredMembers }: MembersTableProps) => {
           <AlertDialogFooter>
             <AlertDialogCancel>Cancelar</AlertDialogCancel>
             <AlertDialogAction onClick={handleConfirmToggle}>
-              {selectedMember && selectedMember.isActive
+              {selectedMember && isActive(selectedMember)
                 ? "Desactivar"
                 : "Activar"}
             </AlertDialogAction>
