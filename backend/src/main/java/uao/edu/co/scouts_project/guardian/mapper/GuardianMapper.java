87d4862e251d8fb493904da74d330faf8d0473f1
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2025-10-29 10:13:15 -0500

    fix. removed not implemented TODOs on GuardianMapper

--- backend/src/main/java/uao/edu/co/scouts_project/guardian/mapper/GuardianMapper.java
+++ backend/src/main/java/uao/edu/co/scouts_project/guardian/mapper/GuardianMapper.java
@@ -34,7 +34,7 @@ public class GuardianMapper {
                 .build();
     }
 
-    // Convert Member entity to GuardianWithMemberDTO usually for returning guardian info with members they are in charge of (TODO: this have to be implemented in the service layer)
+    // Convert Member entity to GuardianWithMemberDTO usually for returning guardian info with members they are in charge of
     public static GuardianWithMembersDTO toDTO(Member entity, List<MemberDTO> membersInCharge) {
         return GuardianWithMembersDTO.builder()
                 .userId(entity.getUserId())
@@ -53,7 +53,7 @@ public class GuardianMapper {
                 .relationship(entity.getRelationship())
                 .status(entity.getStatus())
                 .acceptanceDate(entity.getAcceptanceDate())
-                .members(membersInCharge) // TODO: We need to create a domain model in our entity so it bring us a list of members based on guardianId, in other words, the member a guardian is taking care of, then we map it to List <MemberDTO>
+                .members(membersInCharge)
                 .build();
     }
 
